a7c51dc4de965b41f2ec663a373e4d93a02521d8
Anton Mihail Lachmaniucu <anton2mihail@users.noreply.github.com>
2018-11-15 18:22:55 -0500

    Update text.js

--- routes/text.js
+++ routes/text.js
@@ -8,19 +8,19 @@ const authToken = process.env.AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 const FRONT_URL = process.env.FRONTEND_PUBLIC_URL;
 
-/*function handleInboundSms(request, response) {
+function handleInboundSms(request, response) {
   const params = Object.assign(request.query, request.body)
   console.log(params.Body);
-  fetch(FRONT_URL, {
+ /* fetch(FRONT_URL, {
     method: 'POST',
     body: JSON.stringify({
       eta: params.Body
     })
   }).then((res) => {
     console.log("Eta Sent");
-  });
+  });*/
   response.status(204).send()
-}*/
+}
 
 module.exports = (() => {
   router.post('/', (req, res) => {
@@ -44,7 +44,7 @@ module.exports = (() => {
       });
     })
   });
- // router.post('/inbound-sms', handleInboundSms);
+ router.post('/inbound-sms', handleInboundSms);
 
 
 
